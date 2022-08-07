5d0784417f4280cb194f1d76badca8b77ca95b3b
fvirtuozo <108958483+fvirtuozo@users.noreply.github.com>
2022-08-07 23:00:24 +0100

    added url patterns, created about/contact page

--- src/e_commerce/views.py
+++ src/e_commerce/views.py
@@ -1,5 +1,24 @@
 
+from multiprocessing import context
 from django.http import HttpResponse
 from django.shortcuts import render
 def home_page(request):
-    return render(request, "home_page.html", {})
+    context= {
+        "title": "Home Page"
+        "content": "Welcome to the home page"
+    }
+    return render(request, "home_page.html", context)
+
+def about_page(request):
+    context= {
+        "title": "About"
+        "content": "Welcome to About page"
+    }
+    return render(request, "about_page.html", context)
+
+    def contact_page(request):
+    context= {
+        "title": "Contact"
+        "content": "Welcome to the Contact page"
+    }
+    return render(request, "contact_page.html", context)
