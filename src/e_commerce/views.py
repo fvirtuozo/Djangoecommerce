61f52ef10e7947189548d36db9980e9b6e93d7f1
fvirtuozo <108958483+fvirtuozo@users.noreply.github.com>
2022-08-07 23:33:24 +0100

    added title and content dictionary content to home page

--- src/e_commerce/views.py
+++ src/e_commerce/views.py
@@ -2,23 +2,25 @@
 from multiprocessing import context
 from django.http import HttpResponse
 from django.shortcuts import render
+
 def home_page(request):
     context= {
-        "title": "Home Page"
-        "content": "Welcome to the home page"
+        'title': 'Home Page',
+        'content': 'Welcome to the home page'
     }
     return render(request, "home_page.html", context)
 
 def about_page(request):
     context= {
-        "title": "About"
-        "content": "Welcome to About page"
+        'title': 'About',
+        'content': 'Welcome to About page'
     }
-    return render(request, "about_page.html", context)
+    return render(request, "about/view.html", context)
 
-    def contact_page(request):
-    context= {
-        "title": "Contact"
-        "content": "Welcome to the Contact page"
+    
+def contact_page(request):
+    context = {
+        'title': 'Contact', 
+        'content': 'Welcome to the Contact page'
     }
-    return render(request, "contact_page.html", context)
+    return render(request, "contact/view.html", context)
