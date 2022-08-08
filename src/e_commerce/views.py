213d322770eba1652a801649566948d857046f22
fvirtuozo <108958483+fvirtuozo@users.noreply.github.com>
2022-08-08 11:52:23 +0100

    update form contact

--- src/e_commerce/views.py
+++ src/e_commerce/views.py
@@ -3,6 +3,8 @@ from multiprocessing import context
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from .forms import ContactForm
+
 def home_page(request):
     context= {
         'title': 'Home Page',
@@ -18,8 +20,17 @@ def about_page(request):
     return render(request, "about/view.html", context)
    
 def contact_page(request):
+    contact_form = ContactForm(request.POST or None)
     context = {
-        'title': 'Contact', 
-        'content': 'Welcome to the Contact page'
+    
+        "title": "Página de contato",
+        "content": "Bem-vindo a página de contato",
+        "form": contact_form
     }
+   
+    if request.method == "POST":
+        print(request.POST)
+        print(request.POST.get('Nome_Completo'))
+        print(request.POST.get('email'))
+        print(request.POST.get('Mensagem'))
     return render(request, "contact/view.html", context)
