d223f1c5488f9dd6fe13879488df39d0ba0769c3
fvirtuozo <108958483+fvirtuozo@users.noreply.github.com>
2022-08-08 22:31:06 +0100

    import cbv fbv product views.py

--- src/e_commerce/urls.py
+++ src/e_commerce/urls.py
@@ -18,6 +18,7 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 from .views import home_page, about_page, contact_page, login_page, register_page
+from products.views import ProductListView, product_list_view
 
 urlpatterns = [
     path('', home_page),
@@ -25,7 +26,10 @@ urlpatterns = [
 	path('contact/', contact_page),
     path('login/', login_page),
     path('register/', register_page),
+    path('products/', ProductListView.as_view()),
+    path('products-fbv/',products_list_view),
     path('admin/', admin.site.urls),
+
 ]
 
 if settings.DEBUG:
