3baa25617010bc0c4a1e40af150625cb99813cd3
fvirtuozo <108958483+fvirtuozo@users.noreply.github.com>
2022-08-08 17:25:29 +0100

    inseted static files in settings.py

--- src/e_commerce/forms.py
+++ src/e_commerce/forms.py
@@ -1,5 +1,7 @@
 from django import forms
+from django.contrib.auth import get_user_model
 
+User = get_user_model()
 class ContactForm(forms.Form):
     full_name = forms.CharField(
         widget=forms.TextInput(
@@ -25,8 +27,41 @@ class ContactForm(forms.Form):
                 }
             )
         )
+    
     def clean_email(self):
         email = self.cleaned_data.get("email")
         if not "gmail.com" in email:
             raise forms.ValidationError("O Email deve ser do gmail.com")
         return email
+
+class LoginForm(forms.Form):
+    username = forms.CharField()
+    password = forms.CharField(widget=forms.PasswordInput)
+
+class RegisterForm(forms.Form):
+    username = forms.CharField()
+    email = forms.EmailField()
+    password = forms.CharField(widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Confirm password', widget=forms.PasswordInput)
+
+    def clean_username(self):
+        username = self.cleaned_data.get('username')
+        qs = User.objects.filter(username=username)
+        if qs.exists():
+            raise forms.ValidationError("This user already exists, please choose another.")
+        return username
+    
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        qs = User.objects.filter(email=email)
+        if qs.exists():
+            raise forms.ValidationError("This email already exists, try another!")
+        return email
+
+    def clean(self):
+        data = self.cleaned_data
+        password = self.cleaned_data.get('password')
+        password2 = self.cleaned_data.get('password2')
+        if password != password2:
+            raise forms.ValidationError("The passwords informed must be equals!")
+        return data
