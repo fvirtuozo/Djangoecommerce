9dca5b9fa5a3a857a7983e651bcc08e0036498d6
fvirtuozo <108958483+fvirtuozo@users.noreply.github.com>
2022-08-09 15:47:13 +0100

    update views.py and create detail.html

--- src/products/views.py
+++ src/products/views.py
@@ -1,6 +1,6 @@
 
-from django.views.generic import ListView
-from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from django.shortcuts import render, get_object_or_404
 
 from .models import Product
 
@@ -23,4 +23,24 @@ def product_list_view(request):
     }
     return render(request, "products/list.html", context)
 
+#Class Based View
+class ProductDetailView(DetailView):
+    #traz todos os produtos do banco de dados sem filtrar nada 
+    queryset = Product.objects.all()
+    template_name = "products/detail.html"
+    
+    def get_context_data(self, *args, **kwargs):
+        context = super(ProductDetailView, self).get_context_data(*args, **kwargs)
+        print(context)
+        return context
+
+#Function Based View
+def product_detail_view(request, pk=None, *args, **kwargs):
+    #instance=Product.objects.get(pk=pk) #get object id
+    instance=get_object_or_404(Product, pk=pk)
+    queryset = Product.objects.all()
+    context = {
+        'object': instance
+    }
+    return render(request, "products/detail.html", context)
    
